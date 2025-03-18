455d70fd6b738ce0e8c87f1b17cab18dd950237d
Vansh Narang <xs522-vannar@xs-host603-055.xs.neuralcompany.team>
2025-03-18 22:01:04 +0530

    Test 6 improved

--- e2e/test-6.spec.ts
+++ e2e/test-6.spec.ts
@@ -1,25 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-
 //Check all links in the website
 
 test("Check for broken links", async ({ page, context }) => {
-    test.setTimeout(120000);
-    await page.goto("https://www.xenonstack.com/");
+  test.setTimeout(120000);
+  await page.goto("https://www.xenonstack.com/");
 
-    //Getting all links of the page
-    const links = await page.$$eval("a", (anchors) =>
-        anchors.map((a) => a.href).filter((href) => href.startsWith("http"))
-    );
+  //Getting all links of the page
+  const links = await page.$$eval("a", (anchors) =>
+    anchors.map((a) => a.href).filter((href) => href.startsWith("http")),
+  );
 
-    console.log(`Total links found: ${links.length}`);
+  console.log(`Total links are: ${links.length}`);
 
-    for (const link of links) {
-        const response = await context.request.get(link);
-        if (!response.ok()) {
-            console.warn(`Broken Link: ${link} (Status: ${response.status()})`);
-        } else {
-            console.log(`Valid Link: ${link} (Status: ${response.status()})`);
-        }
+  for (const link of links) {
+    try {
+      const response = await context.request.get(link);
+      if(response.ok()){
+        console.log(`Valid Link: ${link}`);
+      }
+    } catch (error) {
+        console.warn(`Broken Link: ${link} ${error}`);
     }
+  }
 });
